6fbed575011f9de38eebabfa3c86ec6f373935ed
James Crosslin <jamesmscrosslin@gmail.com>
2021-03-19 14:57:23 -0700

    added Stats component

---
+++ src/components/Stats.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Stats(props) {
+  const { totalPlayers, totalScore } = props;
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{totalScore}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Stats;
