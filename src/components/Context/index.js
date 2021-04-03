d3e934c1998389f97ece0eddda7454d84626f335
James Crosslin <jamesmscrosslin@gmail.com>
2021-04-02 18:11:08 -0700

    Revert "removed artifact"
    This reverts commit 2509dea32a0f222838588c322b61a7284dcdcdc4.

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
+import PlayerData from "../../interfaces/PlayerData";
 
 const PlayersContext = React.createContext({
   players: [],
